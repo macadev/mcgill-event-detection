f9dc9a2955ba085a75153ab66ea6cb84c746f769
macadev <danielmacario5@gmail.com>
2016-02-02 15:05:46 -0500

    add list comprehesion to fix parsing issue

--- src/EventDetectionWebServer/EventDetectionWebServer.py
+++ src/EventDetectionWebServer/EventDetectionWebServer.py
@@ -111,7 +111,7 @@ def process_predict():
 	        #coordinates_roi = map(int, coordinates_roi)
             coordinates_roi_list = [float(number) for number in predict_attr.get('points').split(',')]
             it = iter(coordinates_roi_list)
-            coordinates_roi = zip(it, it)
+            coordinates_roi = [list(elem) for elem in zip(it, it)]
             time_roi = float(predict_attr.get('time'))
 
             # TODO: Obtain the coordinates of the mask through OpenCV
